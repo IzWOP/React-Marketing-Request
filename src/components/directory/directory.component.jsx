80c1b3075739d598160bfd5e3acc211efd2c69d7
Izzy <izzyinjurious@aim.com>
2020-04-17 07:10:01 -0700

    started directory and react app

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Directory extends React.Component {
+	state = {
+		sections: [
+			{
+				title: 'Design',
+				id: 1,
+			},
+			{
+				title: 'Content',
+				id: 2,
+			},
+			{
+				title: 'Event',
+				id: 3,
+			},
+		],
+	};
+}
